7887e1df6a6e8b269ce3da1c604877e65526ced7
jacobduncan00 <jacobduncan00@yahoo.com>
2022-05-05 09:55:30 -0400

    Update

--- api-entrypoint.sh
+++ api-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf migrations
 flask db init
 flask db migrate
 flask db upgrade
--- db-entrypoint.sh
+++ db-entrypoint.sh
@@ -2,5 +2,4 @@
 
 psql -d postgres -c "CREATE USER postgres WITH PASSWORD 'postgres';" 
 psql -v ON_ERROR_STOP=1 -d postgres -c "CREATE DATABASE internship_portal OWNER 'postgres';"
-pg_ctl -v ON_ERROR_STOP=1 -D "/var/lib/postgres/data" -m fast -w stop
 exec "$@"
